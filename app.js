32592960665eaa4f642565a381b21c8133c571c0
Ashim Rai <thesaarvik@gmail.com>
2023-05-14 20:33:12 +0545

    Some changes made by raman

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const request = require("request");
 const https = require("https");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/signup.html");
+  res.sendFile(path.join(__dirname, "signup.html"));
 });
 
 app.post("/", function (req, res) {
@@ -35,14 +36,14 @@ app.post("/", function (req, res) {
   const url = "https://us21.api.mailchimp.com/3.0/lists/ce1ecb598d";
   const options = {
     method: "POST",
-    auth: "ashim:3efdf37901b594aa085fc35e6f1088e-us21",
+    auth: "ashim:13efdf37901b594aa085fc35e6f1088e-us21",
   };
 
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/sucess.html");
+      res.sendFile(path.join(__dirname, "sucess.html"));
     } else {
-      res.sendFile(__dirname + "/failure.html");
+      res.sendFile(path.join(__dirname, "failure.html"));
     }
 
     response.on("data", function (data) {
@@ -58,7 +59,9 @@ app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000 || process.env.PORT, function () {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
   console.log("hello");
 });
 
